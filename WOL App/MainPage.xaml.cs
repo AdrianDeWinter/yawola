f4db538460b9cc37f6ae0e4d64b9884ed6083d85
Martin Blecker <martin.blecker@alumni.fh-aachen.de>
2021-08-08 11:24:13 +0200

    Initial push of crrent state

---
+++ WOL App/MainPage.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Networking.Sockets;
+using Windows.Networking;
+using Windows.Storage.Streams;
+using System.Collections.ObjectModel;
+
+namespace WOL_App
+{
+    public sealed partial class MainPage : Page
+    {
+        private readonly bool debug = true;
+
+        private readonly DatagramSocket socket = new DatagramSocket();
+
+        public readonly ObservableCollection<WolTarget> targets = new ObservableCollection<WolTarget>();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            TargetList.ItemsSource = targets;
+            if (debug)
+                targets.Add(new WolTarget("192.168.188.128", "84:D8:1B:60:D6:AE", "mveServer", "9999"));
+        }
+
+        private async void Send_Button_Click(object sender, RoutedEventArgs e)
+        {
+            WolTarget target = (WolTarget)TargetList.SelectedItem;
+            if (debug)
+            {
+                Debug.WriteLine("Sending packet:");
+                Debug.WriteLine(target.ToString());
+            }
+
+            //get out stream to the selected target
+            IOutputStream outStream = null;
+            try
+            {
+                outStream = await socket.GetOutputStreamAsync(target.HostName, target.Port);
+            }
+            catch(Exception except)
+            {
+                Debug.WriteLine("Send failed with error: " + except.Message);
+                return;
+            }
+            //obtain writer for the stream
+            DataWriter writer = new DataWriter(outStream);
+            //write the packet to the stream
+            if (debug)
+                Debug.WriteLine("writing data");
+            writer.WriteBytes(target.MagicPacket());
+            //send the packet
+            try
+            {
+                await writer.StoreAsync();
+            }
+            catch (Exception except)
+            {
+                Debug.WriteLine("Send failed with error: " + except.Message);
+                return;
+            }
+            if (debug)
+                Debug.WriteLine("Packet sent");
+        }
+
+        private void Add_Button_Click(object sender, RoutedEventArgs e)
+        {
+            WolTarget target = new WolTarget(ipInput.Text, macInput.Text, clientNameInput.Text, "9999");
+            targets.Add(target);
+            if (debug)
+            {
+                Debug.WriteLine("Saved target:");
+                Debug.WriteLine(target.HostName);
+                Debug.WriteLine(target.Mac_string);
+                Debug.WriteLine(target.Port);
+            }
+        }
+
+        private void Delete_Button_Click(object sender, RoutedEventArgs e)
+        {
+            targets.Remove((WolTarget)TargetList.SelectedItem);
+        }
+    }
+}
---
+++ WOL App/wolTarget.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Networking;
+
+namespace WOL_App
+{
+    public struct WolTarget
+    {
+        public string Address { get; private set; }
+        public string Port { get; private set; }
+        public string Mac_string { get; private set; }
+        public byte[] Mac { get; private set; }
+        public HostName HostName { get; private set; }
+
+        public string Name { get; private set; }
+
+        public WolTarget(string address, string mac, string name, string port = "0")
+        {
+            Address = "";
+            HostName = null;
+            Port = port;
+            Mac = new byte[102];
+            Mac_string = "";
+            Name = name;
+            SetAddress(address);
+            SetMac(mac);
+        }
+        public void SetAddress(string address)
+        {
+            Address = address;
+            HostName = new HostName(address);
+        }
+
+        public void SetMac(string mac)
+        {
+            Mac_string = mac;
+            //split the mc address string into its byte strings
+            string[] macArray = mac.Split(':');
+
+            Mac = new byte[6];
+
+            //parse the mac's byte strings into a bytes in an array
+            for (int i = 0; i < 6; i++)
+                Mac[i] = byte.Parse(macArray[i], System.Globalization.NumberStyles.HexNumber);
+        }
+
+        public void SetPort(string port)
+        {
+            Port = port;
+        }
+
+        public byte[] MagicPacket()
+        {
+            byte[] packet = new byte[102];
+
+            //wite the sync stream into the packet, and parse the mac's byte strings into a bytes in an array
+            for (int i = 0; i < 6; i++)
+                packet[i] = 0xff;
+
+            //write 16 copies of the mac address into the packet
+            for (int i = 6; i < 102; i += 6)
+                for (int j = 0; j < 6; j++)
+                    packet[i + j] = Mac[j];
+
+            return packet;
+        }
+
+        public override string ToString()
+        {
+            return "Display Name: " + Name + "\nAddress: " + Address + "\nMAC: " + Mac_string;
+        }
+    }
+}
