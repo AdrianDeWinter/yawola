abc174f0ad3cb6ed01c689a530b4501db84a64f2
Martin Blecker <martin.blecker@alumni.fh-aachen.de>
2021-08-10 22:35:46 +0200

    moved the logic for sending magic packets into the wolTarget class

--- WOL App/MainPage.xaml.cs
+++ WOL App/MainPage.xaml.cs
@@ -3,17 +3,13 @@ using System.Diagnostics;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.Networking.Sockets;
-using Windows.Storage.Streams;
 using System.Collections.ObjectModel;
-using Microsoft.Toolkit.Uwp.UI;
 
 namespace WOL_App
 {
     public sealed partial class MainPage : Page
     {
         private readonly bool debug = true;
-
-        private readonly DatagramSocket socket = new DatagramSocket();
 
         public readonly ObservableCollection<WolTarget> targets = new ObservableCollection<WolTarget>();
 
@@ -25,49 +21,20 @@ namespace WOL_App
                 targets.Add(new WolTarget("192.168.188.128", "84:D8:1B:60:D6:AE", "mveServer", "9999"));
         }
 
-        private async void Send_Button_Click(object sender, RoutedEventArgs e)
+        private void Send_Button_Click(object sender, RoutedEventArgs e)
         {
             WolTarget target = (WolTarget)TargetList.SelectedItem;
-            if (debug)
-            {
-                Debug.WriteLine("Sending packet:");
-                Debug.WriteLine(target.ToString());
-            }
-
-            //get out stream to the selected target
-            IOutputStream outStream = null;
-            try
-            {
-                outStream = await socket.GetOutputStreamAsync(target.HostName, target.Port);
-            }
-            catch(Exception except)
-            {
-                Debug.WriteLine("Send failed with error: " + except.Message);
-                return;
-            }
-            //obtain writer for the stream
-            DataWriter writer = new DataWriter(outStream);
-            //write the packet to the stream
-            if (debug)
-                Debug.WriteLine("writing data");
-            writer.WriteBytes(target.MagicPacket());
-            //send the packet
-            try
-            {
-                await writer.StoreAsync();
-            }
-            catch (Exception except)
-            {
-                Debug.WriteLine("Send failed with error: " + except.Message);
-                return;
-            }
-            if (debug)
-                Debug.WriteLine("Packet sent");
+            target.SendMagicPacket(debug);
         }
 
-        private void Add_Button_Click(object sender, RoutedEventArgs e)
+        private void Add_Host(ContentDialog sender, ContentDialogButtonClickEventArgs args)
         {
-            WolTarget target = new WolTarget(ipInput.Text, macInput1.Text, clientNameInput.Text, "9999");
+            string[] mac = {
+                macInput0.Text, macInput1.Text ,
+                macInput2.Text, macInput3.Text ,
+                macInput4.Text, macInput5.Text
+            };
+            WolTarget target = new WolTarget(ipInput.Text, mac, clientNameInput.Text, portInput.Text);
             targets.Add(target);
             if (debug)
             {
@@ -80,32 +47,24 @@ namespace WOL_App
 
         private void Delete_Button_Click(object sender, RoutedEventArgs e)
         {
-            targets.Remove((WolTarget)TargetList.SelectedItem);
-        }
-
-        private void MacInput_OnTextChanged(object sender, TextChangedEventArgs e)
-        {
-            TextBox activeBox = (TextBox)sender;
-            if (!TextBoxExtensions.GetIsValid(activeBox))
-            {
-
-            }
-            switch (activeBox.Name)
-            {
-                case "macInput1": break;
-                case "macInput2": break;
-                case "macInput3": break;
-                case "macInput4": break;
-                case "macInput5": break;
-                case "macInput6": break;
-                default:
-                    break;
-            }
+            _ = targets.Remove((WolTarget)TargetList.SelectedItem);
         }
 
         private async void Open_Add_Dialog(object sender, RoutedEventArgs e)
         {
-            ContentDialogResult result = await addHostDialog.ShowAsync();
+            _ = await addHostDialog.ShowAsync();
+        }
+
+        private void ValidateAddHostForm(object sender, TextChangedEventArgs args)
+        {
+            if (debug)
+                Debug.WriteLine("Validating Form...");
+            //host/ip and display name must be set
+            if (clientNameInput.Text != "" && ipInput.Text != "")
+                addHostDialog.IsPrimaryButtonEnabled = true;
+            addHostDialog.IsPrimaryButtonEnabled = false;
+            //wont check the mac any closer, correctness is suffieciently imposed by the input fields maxLength and Regex
+            //ever field being blank will be parsed to 0x00:0x00:0x00:0x00:0x00:0x00, which is (theoretically) a valid mac
         }
     }
 }
--- WOL App/wolTarget.cs
+++ WOL App/wolTarget.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Networking;
+using System.Diagnostics;
+using Windows.Storage.Streams;
+using Windows.Networking.Sockets;
 
 namespace WOL_App
 {
@@ -28,6 +31,17 @@ namespace WOL_App
             SetAddress(address);
             SetMac(mac);
         }
+        public WolTarget(string address, string[] mac, string name, string port = "0")
+        {
+            Address = "";
+            HostName = null;
+            Port = port;
+            Mac = new byte[102];
+            Mac_string = "";
+            Name = name;
+            SetAddress(address);
+            ParseMac(mac);
+        }
         public void SetAddress(string address)
         {
             Address = address;
@@ -40,11 +54,36 @@ namespace WOL_App
             //split the mc address string into its byte strings
             string[] macArray = mac.Split(':');
 
-            Mac = new byte[6];
+            ParseMac(macArray);
+        }
 
+        private void ParseMac(string[] macSubstrings)
+        {
+            Mac = new byte[6];
+            Mac_string = "";
             //parse the mac's byte strings into a bytes in an array
             for (int i = 0; i < 6; i++)
-                Mac[i] = byte.Parse(macArray[i], System.Globalization.NumberStyles.HexNumber);
+            {
+                //if anything was entered into this field, parse it
+                if (macSubstrings[i] != "")
+                {
+                    //if a single digit was entered into this field, pad with a zero
+                    if (macSubstrings[i].Length < 2)
+                        Mac_string += "0";
+                    Mac_string += macSubstrings[i];
+                    Mac[i] = byte.Parse(macSubstrings[i], System.Globalization.NumberStyles.HexNumber);
+                }
+                //if nothing was entered, default to "00"
+                else
+                {
+                    Mac_string += "00";
+                    Mac[i] = 0;
+                }
+
+                //also add colons to the mac string as it is being built (except after the last byte)
+                if (i < 5)
+                    Mac_string += ":";
+            }
         }
 
         public void SetPort(string port)
@@ -66,6 +105,45 @@ namespace WOL_App
                     packet[i + j] = Mac[j];
 
             return packet;
+        }
+
+        public async void SendMagicPacket(bool debug = false)
+        {
+            if (debug)
+            {
+                Debug.WriteLine("Sending packet:");
+                Debug.WriteLine(ToString());
+            }
+            DatagramSocket socket = new DatagramSocket();
+            //get out stream to the selected target
+            IOutputStream outStream;
+            try
+            {
+                outStream = await socket.GetOutputStreamAsync(this.HostName, this.Port);
+            }
+            catch (Exception except)
+            {
+                Debug.WriteLine("Send failed with error: " + except.Message);
+                return;
+            }
+            //obtain writer for the stream
+            DataWriter writer = new DataWriter(outStream);
+            //write the packet to the stream
+            if (debug)
+                Debug.WriteLine("writing data");
+            writer.WriteBytes(this.MagicPacket());
+            //send the packet
+            try
+            {
+                _ = await writer.StoreAsync();
+            }
+            catch (Exception except)
+            {
+                Debug.WriteLine("Send failed with error: " + except.Message);
+                return;
+            }
+            if (debug)
+                Debug.WriteLine("Packet sent");
         }
 
         public override string ToString()
